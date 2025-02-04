5677c76b87d494d8d83cf3ea7017a68f6b585ce3
Denis Nikolas <d.nikolas@xsolla.com>
2025-02-05 01:00:26 +0500

    Fix frontend

--- js/src/forum/addLoginButton.js
+++ js/src/forum/addLoginButton.js
@@ -3,16 +3,16 @@ import LogInButtons from 'flarum/components/LogInButtons';
 import app from 'flarum/app';
 
 export default function () {
-  const enableWidget = app.forum.attribute('nikovonlas-tg-notify.enableWidget');
-  if (!enableWidget) {
-    return;
-  }
-  const authUrl = app.forum.attribute('baseUrl') + '/auth/telegram';
-  const botUsername = app.forum.attribute('nikovonlas-tg-notify.addWidget');
-  const size = app.forum.attribute('nikovonlas-tg-notify.widgetSize');
-  const radius = app.forum.attribute('nikovonlas-tg-notify.widgetRadius');
-  const userPic = app.forum.attribute('nikovonlas-tg-notify.widgetUserPic');
   extend(LogInButtons.prototype, 'items', function (items) {
+    const enableWidget = app.forum.attribute('nikovonlas-tg-notify.enableWidget');
+    if (!enableWidget) {
+      return;
+    }
+    const authUrl = app.forum.attribute('baseUrl') + '/auth/telegram';
+    const botUsername = app.forum.attribute('nikovonlas-tg-notify.addWidget');
+    const size = app.forum.attribute('nikovonlas-tg-notify.widgetSize');
+    const radius = app.forum.attribute('nikovonlas-tg-notify.widgetRadius');
+    const userPic = app.forum.attribute('nikovonlas-tg-notify.widgetUserPic');
     // Replace the TelegramProvide widget script
     items.add(
       'nikovonlas-tg-notify',
